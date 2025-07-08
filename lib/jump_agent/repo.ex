ae88d42a7fc774a2ce74fca10c55b6b7e0b067a6
Chidiebere Joel <chidiebereyjoel@gmail.com>
2025-07-08 11:13:29 +0100

    Revert Fix

--- lib/jump_agent/repo.ex
+++ lib/jump_agent/repo.ex
@@ -1,9 +1,5 @@
 defmodule JumpAgent.Repo do
   use Ecto.Repo,
-    otp_app: :jump_agent,
-    adapter: Ecto.Adapters.Postgres
-
-  def init(_type, config) do
-    {:ok, Keyword.put(config, :extensions, [{Pgvector.Extensions.Vector, []}])}
-  end
+      otp_app: :jump_agent,
+      adapter: Ecto.Adapters.Postgres
 end
